fc46e8fbbab94d6b4a2f521bcc298b3c31531371
Carl <lundgren.e.carl@gmail.com>
2021-04-20 14:38:50 -0700

    commit tree d7c9ae79cc1e19ad9718d041899588e471b1f124

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const formData = {
+        name: userName,
+        email: userEmail
+    };
+    
+    const configObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body : JSON.stringify(formData)
+    };
+
+    return fetch ('http://localhost:3000/users', configObject)
+        .then(response => response.json())
+        .then(object => document.body.append(object.id))
+        .catch(error => {
+            document.body.append(error.message);
+        });
+}
